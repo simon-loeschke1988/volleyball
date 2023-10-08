c2b581066421dbb01cad6f771c8766323d76cff0
Simon Löschke <simon-loeschke@posteo.de>
2023-10-09 01:16:00 +0200

    Links hinzugefügt

--- applikation/webapp/views.py
+++ applikation/webapp/views.py
@@ -3,10 +3,21 @@ from django.template import loader
 
 from django.http import HttpResponse
 
+# models
+from .models import Players, Courts, Matches, Cities, Teams, Tournament
+
 # Create your views here.
 
 def index(request):
     # index.html is located in applikation/webapp/templates/index.html, but we don't need to specify the full path // Simon Löschke
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
+
+def player(request):
+    player = Players.objects.all().values()
+    template = loader.get_template('player.html')
+    context = {
+        'players': player,
+    }
+    return HttpResponse(template.render(context, request))
 
