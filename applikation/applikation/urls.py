5bff53ebf8c45c9d339e1d7fe05e4addd50b9af5
Simon Löschke <simon-loeschke@posteo.de>
2024-01-07 15:54:53 +0100

    Umstellung auf Flask

--- applikation/applikation/urls.py
+++
@@ -1,9 +0,0 @@
-# Autor: Simon Löschke
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('webapp.urls', 'webapp'), namespace='webapp')),
-    
-]
