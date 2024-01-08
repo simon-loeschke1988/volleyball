ad7ab9268235b692e60bb7ea0f6c2914528822b4
Simon Löschke <simon-loeschke@posteo.de>
2024-01-09 00:00:14 +0100

    Update 'no' field length in Event model

--- applikation/webapp/models.py
+++ applikation/webapp/models.py
@@ -110,7 +110,7 @@ class Event(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
-    no = models.CharField(max_length=50, null=True, blank=True)
+    no = models.CharField(max_length=100, null=True, blank=True)
     version = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
