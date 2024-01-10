64c80b1fa9873137381651882a89486568c4136e
Simon Löschke <simon-loeschke@posteo.de>
2024-01-10 22:41:57 +0100

    lokalisierung geändert

--- applikation/applikation/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-from django.conf import settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applikation.settings')
-
-app = Celery('applikation')
-app.config_from_object(settings, namespace='CELERY')
-app.autodiscover_tasks()
