2683242e448d054ea06300aa0806b0b6d88a3784
Simon Löschke <simon-loeschke@posteo.de>
2024-01-13 13:02:14 +0100

    teams grundsätzlich fertig

--- applikation/webapp/urls.py
+++ applikation/webapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('player/', views.player, name='player'),
-    path('beach_teams/', views.beach_teams_list, name='beach_teams_list'),
+    path('teams/', views.teams, name='teams'),
 ]
