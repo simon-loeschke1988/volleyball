93a49057c4dee136c135a12da6081015825f88e7
Simon Löschke <simon-loeschke@posteo.de>
2024-01-25 00:45:27 +0100

    Fertigstellung

--- applikation/webapp/management/commands/import_matches.py
+++ applikation/webapp/management/commands/import_matches.py
@@ -22,61 +22,58 @@ class Command(BaseCommand):
         data = []
 
         for match in xml_response.findall('BeachMatch'):
-            match_data = {
-                'No': match.attrib.get('No'),
-                'NoInTournament': match.attrib.get('NoInTournament'),
-                'LocalDate': match.attrib.get('LocalDate'),
-                'LocalTime': match.attrib.get('LocalTime'),
-                'NoTeamA': match.attrib.get('NoTeamA'),
-                'NoTeamB': match.attrib.get('NoTeamB'),
-                'Court': match.attrib.get('Court'),
-                'MatchPointsA': match.attrib.get('MatchPointsA'),
-                'MatchPointsB': match.attrib.get('MatchPointsB'),
-                'NoRound': match.attrib.get('NoRound'),
-                'NoTournament': match.attrib.get('NoTournament'),
-            }
+            local_date = match.attrib.get('LocalDate')
+            local_time = match.attrib.get('LocalTime')
 
-            if not any(value is None for value in match_data.values()):
+            # Nur Daten hinzufügen, wenn LocalDate und LocalTime vorhanden sind
+            if local_date and local_time:
+                match_data = {
+                    'No': match.attrib.get('No'),
+                    'NoInTournament': match.attrib.get('NoInTournament'),
+                    'LocalDate': local_date,
+                    'LocalTime': local_time,
+                    'NoTeamA': match.attrib.get('NoTeamA'),
+                    'NoTeamB': match.attrib.get('NoTeamB'),
+                    'Court': match.attrib.get('Court'),
+                    'MatchPointsA': match.attrib.get('MatchPointsA'),
+                    'MatchPointsB': match.attrib.get('MatchPointsB'),
+                    'NoRound': match.attrib.get('NoRound'),
+                    'NoTournament': match.attrib.get('NoTournament'),
+                }
                 data.append(match_data)
 
         df = pd.DataFrame(data)
         df = df.dropna()
         df = df.drop_duplicates(subset=['No'])
-        df = df.astype({'No': 'int32', 'NoInTournament': 'int32', 'NoTeamA': 'int32', 'NoTeamB': 'int32', 'NoRound': 'int32', 'NoTournament': 'int32'})
 
-
-        if not (BeachRound.objects.exists() and BeachTournament.objects.exists()):
-            self.stdout.write(self.style.WARNING('Bitte importieren Sie zuerst die BeachRound und BeachTournament Daten'))
+        if not (BeachRound.objects.exists() and BeachTournament.objects.exists() and BeachTeam.objects.exists()):
+            self.stdout.write(self.style.WARNING('Bitte importieren Sie zuerst die BeachRound, BeachTournament und BeachTeam Daten'))
             return
 
         for index, row in df.iterrows():
-            try:
-                TeamA = BeachTeam.objects.get(no=row['NoTeamA'])
-                TeamB = BeachTeam.objects.get(no=row['NoTeamB'])
-                Round = BeachRound.objects.get(no=row['NoRound'])
-                Tournament = BeachTournament.objects.get(no=row['NoTournament'])
-
+            if row['NoTeamA'] and row['NoTeamB']:
                 try:
+                    TeamA = BeachTeam.objects.get(no=row['NoTeamA'])
+                    TeamB = BeachTeam.objects.get(no=row['NoTeamB'])
+                    Round = BeachRound.objects.get(no=row['NoRound'])
+                    Tournament = BeachTournament.objects.get(no=row['NoTournament'])
 
                     BeachMatch.objects.update_or_create(
-                    No=row['No'],
-                    defaults={
-                        'NoInTournament': row['NoInTournament'],
-                        'LocalDate': pd.to_datetime(row['LocalDate']).date() if pd.notnull(row['LocalDate']) else None,
-                        'LocalTime': pd.to_datetime(row['LocalTime']).time() if pd.notnull(row['LocalTime']) else None,
-                        'NoTeamA': TeamA,
-                        'NoTeamB': TeamB,
-                        'Court': row['Court'],
-                        'MatchPointsA': row['MatchPointsA'],
-                        'MatchPointsB': row['MatchPointsB'],
-                        'NoRound': Round,
-                        'NoTournament': Tournament,
-                    }
+                        No=row['No'],
+                        defaults={
+                            'NoInTournament': row['NoInTournament'],
+                            'LocalDate': pd.to_datetime(row['LocalDate']).date(),
+                            'LocalTime': pd.to_datetime(row['LocalTime']).time(),
+                            'NoTeamA': TeamA,
+                            'NoTeamB': TeamB,
+                            'Court': row['Court'],
+                            'MatchPointsA': row['MatchPointsA'],
+                            'MatchPointsB': row['MatchPointsB'],
+                            'NoRound': Round,
+                            'NoTournament': Tournament,
+                        }
                     )
-                except Exception as e:
-                    self.stdout.write(self.style.WARNING(f'Fehler bein Import {e}'))
-                    continue
-            except (BeachTeam.DoesNotExist, BeachRound.DoesNotExist, BeachTournament.DoesNotExist) as e:
-                self.stdout.write(self.style.WARNING(f'Fehler beim Importieren eines Matches: {e}'))
+                except (BeachTeam.DoesNotExist, BeachRound.DoesNotExist, BeachTournament.DoesNotExist) as e:
+                    self.stdout.write(self.style.WARNING(f'Fehler beim Importieren eines Matches: {e}'))
 
         self.stdout.write(self.style.SUCCESS('BeachMatch Daten erfolgreich importiert.'))
